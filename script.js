3820a4c43108fc7c88dd1027f0ad68bcdc836ab3
ojhaayana-dotcom <ojhaayana@gmail.com>
2026-06-10 19:51:58 +0530

    First version

---
+++ script.js
@@ -0,0 +1,13 @@
+function submitComplaint() {
+
+    let name =
+        document.getElementById("name").value;
+
+    let complaint =
+        document.getElementById("complaint").value;
+
+    console.log(name);
+    console.log(complaint);
+
+    alert("Complaint Submitted!");
+}
