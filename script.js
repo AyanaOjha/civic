939fa56bf924258d1ff4aa6878a4708b190bbd3d
ojhaayana-dotcom <ojhaayana@gmail.com>
2026-06-21 00:03:25 +0530

    Add Hindi language support

--- script.js
+++ script.js
@@ -257,3 +257,67 @@ window.findSchemes = function () {
 document
     .getElementById("filterCategory")
     .addEventListener("change", loadComplaints);
+    function changeLanguage(language) {
+
+    if (language === "hi") {
+
+        document.getElementById("mainTitle")
+            .innerText = "सिविकसेंस";
+
+        document.getElementById("schemeHeading")
+            .innerText = "सरकारी योजना सहायक";
+
+        document.getElementById("dashboardHeading")
+            .innerText = "डैशबोर्ड";
+
+        document.getElementById("complaintsHeading")
+            .innerText = "सभी शिकायतें";
+        document.getElementById("complaintHeading")
+    .innerText = "शिकायत दर्ज करें";
+
+document.getElementById("schemeHeading")
+    .innerText = "सरकारी योजना सहायक";
+
+document.getElementById("filterHeading")
+    .innerText = "शिकायत फ़िल्टर करें";
+    document.getElementById("name")
+    .placeholder = "आपका नाम";
+
+document.getElementById("complaint")
+    .placeholder = "अपनी समस्या बताएं";
+    }
+
+    else {
+
+        document.getElementById("mainTitle")
+            .innerText = "CivicSense";
+
+        document.getElementById("schemeHeading")
+            .innerText = "Government Scheme Assistant";
+
+        document.getElementById("dashboardHeading")
+            .innerText = "Dashboard";
+
+        document.getElementById("complaintsHeading")
+            .innerText = "All Complaints";
+         document.getElementById("complaintHeading")
+    .innerText = "Submit a Complaint";
+
+document.getElementById("schemeHeading")
+    .innerText = "Government Scheme Assistant";
+
+document.getElementById("filterHeading")
+    .innerText = "Filter Complaints";
+    document.getElementById("name")
+    .placeholder = "Your Name";
+
+document.getElementById("complaint")
+    .placeholder = "Describe your issue";
+    }
+}document
+    .getElementById("languageSelect")
+    .addEventListener("change", function () {
+
+        changeLanguage(this.value);
+
+    });
